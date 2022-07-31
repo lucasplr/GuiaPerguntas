da98c93e206e5ab01cb19202a5759bcdbda11432
lucasplr <lucasriffel55@gmail.com>
2022-07-31 17:50:12 -0300

    update

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+//estou dizendo para o express usar o ejs como engine
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+
+    res.render('index')
+})
+
+app.get('/perguntar', (req,res) => {
+    res.render('perguntar')
+})
+
+
+app.listen(8080, () => {
+    console.log('App rodando')
+})
